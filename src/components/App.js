853fc34d2773f0389cd69ebc0e7a6f1a7786f161
shivamcs <shivamcs0049@gmail.com>
2019-06-26 23:40:47 +0530

    menu component for default home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Menu from './Menu.js'
 
 
-class App extends React.Component{
 
-    render(){
-        return(
-            <h1>Productivity Suit</h1>
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Header Will Come Here</h1>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3">
+                           <Menu/> 
+                        </div>
+                        <div className="col-md-9">
+                            <div class="card">
+                                <div class="card-body">
+                                   <h1>Component Will Come Here.</h1>
+                                   <h1>Component Will Come Here.</h1>
+                                   <h1>Component Will Come Here.</h1>
+                                   <h1>Component Will Come Here.</h1>
+                                   
+                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Menu extends React.Component {
+
+    render() {
+        return (
+            <ul className="list-group">
+                <li className="list-group-item "><span class="oi oi-plus">Add Categories</span></li>
+                <li className="list-group-item">Calender</li>
+               
+            </ul>
+        )
+    }
+}
+
+export default Menu;
