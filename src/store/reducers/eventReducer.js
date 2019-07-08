c122b3c3ce2ecb9a45a0c7371402a57dfc291ce4
shivamcs <shivamcs0049@gmail.com>
2019-07-08 10:12:11 +0530

    added edit_event action in event Reducer

--- src/store/reducers/eventReducer.js
+++ src/store/reducers/eventReducer.js
@@ -21,46 +21,6 @@ function eventReducer(events = [], action) {
         return eventState;
     }
 
-    if (action.type === 'EDIT_EVENT_TITLE') {
-
-        let newTitle = {
-            id: action.payLoadData.id,
-            title: action.payLoadData.title
-        }
-
-        let index = events.findIndex((event) => {
-            return event.id === newTitle.id;
-        })
-        let event = Object.assign({}, events[index]);
-        event.eventTitle = newTitle.title
-
-        let newEventsArray = Object.assign([], events);
-        newEventsArray[index] = event;
-
-      
-        return newEventsArray;
-
-    }
-
-    if(action.type === 'EDIT_EVENT_DESCRIPTION'){
-        
-        let newDescription ={
-            id:action.payLoadData.id,
-            description:action.payLoadData.description
-        }
-        // console.log(newDescription);
-        let index = events.findIndex((event)=>{
-            return event.id === newDescription.id;
-        })
-        let event = Object.assign({},events[index]);
-        event.description = newDescription.description;
-
-        let newEventsArray = Object.assign([],events);
-        newEventsArray[index] = event;
-
-        return newEventsArray;
-    }
-
     if(action.type === 'EDIT_EVENT_CHECKBOX'){
         
         let eventStatus ={
